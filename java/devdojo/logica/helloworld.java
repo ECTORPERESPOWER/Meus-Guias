629c0e6575e1110dab40b2f38e074986388069dd
unfreak <91734857+unfreak@users.noreply.github.com>
2021-10-03 19:42:51 +0100

    Create helloworld.java

---
+++ java/devdojo/logica/helloworld.java
@@ -0,0 +1,19 @@
+/*
+public = classe publica
+class = indicando que e uma classe
+helloworld = nome da classe principal
+{} = inicio { | e fim } do bloco de codigo
+; = fim da linha
+system.out.println("printa algo na tela")
+"" = indica uma string
+
+void = não retorna nada
+main = metodo principal (acho que sempre vai usar ele não pode outro nome)
+*/
+
+public class helloworld {
+public static void main (string[]args){
+  system.out.println("DALE MUNDO");
+}
+
+}
