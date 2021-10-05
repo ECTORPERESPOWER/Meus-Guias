0258d1fe884abf38f1a6006226789bb9103813ae
unfreak <91734857+unfreak@users.noreply.github.com>
2021-10-05 21:45:08 +0100

    Create CalculadoraImposto.java

---
+++ java/devdojo/logica/exercicios/CalculadoraImposto.java
@@ -0,0 +1,21 @@
+public class CalculadoraImposto {
+    public static void main(String[]args){
+        float salary = 2500.00F;
+        float percentage = salary * 0.5F;
+        float finalPercentage = percentage;
+        System.out.println(finalPercentage + salary);
+
+
+    }
+}
+
+/*
+    exercicio valores literais
+    
+Crie uma classe que calcule uma determinada porcentagem de um dado salário
+Criar uma classe
+Aceitar um valor de entrada para o salario
+Definir o valor da porcentagem
+Calcular a porcentagem
+
+ */
