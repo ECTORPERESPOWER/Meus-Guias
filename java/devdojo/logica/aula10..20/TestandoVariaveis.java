a31ff74fd39a1324f557a41e70343ea0c99a8af1
unfreak <91734857+unfreak@users.noreply.github.com>
2021-10-06 22:48:20 +0100

    Create TestandoVariaveis.java

---
+++ java/devdojo/logica/aula10..20/TestandoVariaveis.java
@@ -0,0 +1,32 @@
+public class TestandoVariaveis {
+    public static void main(String[] args) {
+        char male = 'M';
+        char female = 'F';
+        String name = "here you can put a lot of words";
+        boolean conditional = false;
+        System.out.println(male + female);
+        System.out.println(conditional);
+        System.out.println("my name is" + name );
+
+
+    }
+
+
+}
+
+
+
+
+/*
+Anotaçoes aleatorias:
+
+[Tipo]  [representaçao] [tamanho] [min] [max]
+Boolean [true or false] [1 bits] [n.a] [n.a]
+char [character unicode] [16 bits] [""] [\uFFFF]
+byte [inteiro com sinal] [8 bits] [-128] [128]
+short [inteiros com sinal] [16 bits] [-32768] [32767]
+int [inteiro com sinal] [32 bits] [-2147483648] [2147483647]
+long [inteiro com sinal] [64 bits] [-9223372036854775808] [9223372036854775807]
+float [numero com ponto flutuante de precissão simples] [32 bits] [+_ 3.40282347E+38] [+_1.40239846E-45]
+double [ numero com ponto flutuante de precissão simples] [64 bits] [+_1.79769313486231570E+308][Loucura]
+ */
