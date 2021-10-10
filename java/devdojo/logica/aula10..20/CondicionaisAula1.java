578143e66be59f703628b22c7a8139640328bdb6
Yonlu-ai <91734857+Yonlu-ai@users.noreply.github.com>
2021-10-10 21:02:17 +0100

    Create CondicionaisAula1.java

---
+++ java/devdojo/logica/aula10..20/CondicionaisAula1.java
@@ -0,0 +1,10 @@
+public class CondicionaisAula1 {
+    public static void main(String[] args) {
+        boolean condition = true;
+        if(condition){
+            System.out.println("Dentro do IF");
+        }else {
+            System.out.println("Fora do if");
+        }
+    }
+}
