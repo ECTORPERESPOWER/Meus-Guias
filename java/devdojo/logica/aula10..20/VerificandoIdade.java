046b7f60f144a6b29075a0284f3d027bb6445eaf
Yonlu-ai <91734857+Yonlu-ai@users.noreply.github.com>
2021-10-10 21:30:45 +0100

    Create VerificandoIdade.java

---
+++ java/devdojo/logica/aula10..20/VerificandoIdade.java
@@ -0,0 +1,19 @@
+public class VerificandoIdade {
+    public static void main(String[] args) {
+        int age = 15;
+        boolean test = true;
+        String name = "Goku";
+        // ==, !=, >, >=,<, <=
+        if(age <= 17) {
+            System.out.println("Ainda não e adulto");
+        }else{
+            System.out.println("Adulto");
+        }
+    }
+}
+
+/*
+Receba uma idade como entrada
+se idade for maior que 18 imprima "Adulto"
+Senão imprima "Ainda não e adulto"
+ */
